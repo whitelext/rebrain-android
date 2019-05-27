49360481c0d2eea1c4188d4af42103d25c3024e0
whitelext <whitelext@gmail.com>
2019-05-28 01:36:42 +0300

    Splash fix

--- FoodApp/app/src/main/java/screen/splash/SplashActivity.kt
+++ FoodApp/app/src/main/java/screen/splash/SplashActivity.kt
@@ -5,22 +5,26 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import com.example.foodapp.MainActivity
 import com.example.foodapp.R
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
+import kotlinx.coroutines.*
+import kotlin.coroutines.CoroutineContext
 
-class SplashActivity : AppCompatActivity() {
+// The Splash which is displayed before the launch of MainActivity
+
+class SplashActivity : AppCompatActivity(), CoroutineScope {
+    private val job = SupervisorJob()
+    override val coroutineContext: CoroutineContext
+        get() = Dispatchers.Main + job
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
         val intent = Intent(this,MainActivity::class.java)
-        GlobalScope.launch(context = Dispatchers.Main) {
-            delay(500)
-            startActivity(intent)
-        }
-
+        navigateToActivityWithDelay(intent,500)
     }
 
+    private fun navigateToActivityWithDelay(intent : Intent,delayTime : Long) = launch {
+        delay(delayTime)
+        startActivity(intent)
+    }
 }
+
