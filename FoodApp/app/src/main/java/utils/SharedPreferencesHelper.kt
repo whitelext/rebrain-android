77824d548aedbed034010b420f68506e03968abd
whitelext <whitelext@gmail.com>
2019-06-10 01:26:09 +0300

    dependencies

--- FoodApp/app/src/main/java/utils/SharedPreferencesHelper.kt
+++ FoodApp/app/src/main/java/utils/SharedPreferencesHelper.kt
@@ -2,7 +2,6 @@ package utils
 
 import android.content.Context
 import android.preference.PreferenceManager
-
 const val introSharedPrefKey = "isIntroNeeded" // Key for Intro to work with Shared Preferences
 /**
  * Helper class to work with SharedPreferences
@@ -13,7 +12,6 @@ const val introSharedPrefKey = "isIntroNeeded" // Key for Intro to work with Sha
  */
 class SharedPreferencesHelper(context : Context) {
     private val sharedPref = PreferenceManager.getDefaultSharedPreferences(context)
-
     var isIntroNeeded: Boolean
         get() = sharedPref.getBoolean(introSharedPrefKey,true)
         set(value) = sharedPref.edit().putBoolean(introSharedPrefKey, value).apply()
