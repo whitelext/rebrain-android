bdb4dd3ce31947c4f303bd5d2c46d725e7873a95
whitelext <whitelext@gmail.com>
2019-06-21 01:26:03 +0300

    Two fragments created

---
+++ FoodApp/app/src/main/java/screen/main/FavouriteFragment.kt
@@ -0,0 +1,30 @@
+package screen.main
+
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.foodapp.R
+import utils.BaseFragment
+
+/**
+ * A [Fragment] subclass for showing favourite food
+ *
+ */
+class FavouriteFragment : BaseFragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_favourite, container, false)
+    }
+
+    companion object {
+        fun newInstance() :FavouriteFragment{
+            return FavouriteFragment()
+        }
+    }
+}
