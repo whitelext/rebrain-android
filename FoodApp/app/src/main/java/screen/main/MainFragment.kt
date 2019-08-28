d7e5ddfae60f52c2970e7e0825494e5735e92797
Aleksei <whitelext@gmail.com>
2019-08-28 14:14:24 +0300

    some fixes

--- FoodApp/app/src/main/java/screen/main/MainFragment.kt
+++ FoodApp/app/src/main/java/screen/main/MainFragment.kt
@@ -31,25 +31,26 @@ class MainFragment : BaseFragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-       initRv(inflater.inflate(R.layout.fragment_main, container, false))
-        return inflater.inflate(R.layout.fragment_main, container, false)
+        val rootView = inflater.inflate(R.layout.fragment_main, container, false)
+       initRv(rootView)
+        return rootView
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val pictures = listOf(
-            R.drawable.img_carousel_1,
-            R.drawable.img_carousel_2,
-            R.drawable.img_carousel_3,
-            R.drawable.img_carousel_4,
-            R.drawable.img_carousel_5,
-            R.drawable.img_carousel_6,
-            R.drawable.img_carousel_7,
-            R.drawable.img_carousel_8,
-            R.drawable.img_carousel_9,
-            R.drawable.img_carousel_10
-        )
-        pageAdapter = CarouselStatePageAdapter(childFragmentManager,pictures)
+//        val pictures = listOf(
+//            R.drawable.img_carousel_1,
+//            R.drawable.img_carousel_2,
+//            R.drawable.img_carousel_3,
+//            R.drawable.img_carousel_4,
+//            R.drawable.img_carousel_5,
+//            R.drawable.img_carousel_6,
+//            R.drawable.img_carousel_7,
+//            R.drawable.img_carousel_8,
+//            R.drawable.img_carousel_9,
+//            R.drawable.img_carousel_10
+//        )
+//        pageAdapter = CarouselStatePageAdapter(childFragmentManager,pictures)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
