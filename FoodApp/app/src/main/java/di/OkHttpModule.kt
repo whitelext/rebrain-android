12d8edc7fad04caeda58428b531d1bbb10a49831
Aleksei <whitelext@gmail.com>
2020-02-17 18:31:16 +0300

    interceptor refactor

--- FoodApp/app/src/main/java/di/OkHttpModule.kt
+++ FoodApp/app/src/main/java/di/OkHttpModule.kt
@@ -2,10 +2,8 @@ package di
 
 import dagger.Module
 import dagger.Provides
-import okhttp3.Interceptor
 import okhttp3.OkHttpClient
-import okhttp3.Response
-import timber.log.Timber
+import utils.LogInterceptor
 
 /**
  * Provides [OkHttpClient] for application
@@ -16,25 +14,6 @@ class OkHttpModule {
 
     @Provides
     @PerApplication
-    fun provideOkHttpClient() = OkHttpClient().newBuilder().addInterceptor(
-        object : Interceptor {
-            override fun intercept(chain: Interceptor.Chain): Response {
-                val request = chain.request()
-                val t1 = System.nanoTime()
-                Timber.tag("Network").i(
-                    "Sending request %s on %s%n%s",
-                    request.url, chain.connection(), request.headers
-                )
-                val response = chain.proceed(request)
-                val t2 = System.nanoTime()
-                Timber.tag("Network").i(
-                    "Received response for %s in %.1fms%n%s",
-                    response.request.url, (t2 - t1) / 1e6, response.headers
-                )
-                return response
-            }
-
-        }
-    ).build()
+    fun provideOkHttpClient() = OkHttpClient().newBuilder().addInterceptor(LogInterceptor()).build()
 
 }
---
+++ FoodApp/app/src/main/java/utils/LogInterceptor.kt
@@ -0,0 +1,28 @@
+package utils
+
+import okhttp3.Interceptor
+import okhttp3.Response
+import timber.log.Timber
+
+/**
+ * Okhttp Interceptor that logs the outgoing request and the incoming response
+ *
+ */
+class LogInterceptor : Interceptor{
+    override fun intercept(chain: Interceptor.Chain): Response {
+        val request = chain.request()
+        val t1 = System.nanoTime()
+        Timber.tag("Network").i(
+            "Sending request %s on %s%n%s",
+            request.url, chain.connection(), request.headers
+        )
+        val response = chain.proceed(request)
+        val t2 = System.nanoTime()
+        Timber.tag("Network").i(
+            "Received response for %s in %.1fms%n%s",
+            response.request.url, (t2 - t1) / 1e6, response.headers
+        )
+        return response
+    }
+
+}
